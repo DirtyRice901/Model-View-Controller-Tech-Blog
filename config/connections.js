f54279ed38976c2222d2bb43626c3270796a532e
Kevin Rice <reatriniti@gmail.com>
2023-09-30 18:11:40 -0400

    connections.js completed and .env file completed

--- config/connections.js
+++ config/connections.js
@@ -2,5 +2,14 @@ const Sequelize = require('sequelize');
 ///////// Import sensitive data from .env //////////////////////////////////////////////////////////////////////////////////////////////////
 require('dotenv').config();
 
-con
-
+//Create a sequelize instance with the database connection details
+let sequelize = new Sequelize(
+    process.env.DB_NAME,
+    process.env.DB_USER,
+    process.env.DB_PASSWORD,
+    {
+    host: 'localhost',    //host name
+    dialect: 'mysql',    //'mysql' ,'mariadb', 'postgres','mssql'
+    port: 3306.          //port number
+    });
+    module.exports = sequelize;    //Export the sequelize object
