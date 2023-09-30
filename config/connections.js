a9ecabfb0af162c7a20c97639cd144391b786f75
Kevin Rice <reatriniti@gmail.com>
2023-09-30 18:30:12 -0400

    connections.js is complete

--- config/connections.js
+++ config/connections.js
@@ -2,14 +2,15 @@ const Sequelize = require('sequelize');
 ///////// Import sensitive data from .env //////////////////////////////////////////////////////////////////////////////////////////////////
 require('dotenv').config();
 
-//Create a sequelize instance with the database connection details
-let sequelize = new Sequelize(
-    process.env.DB_NAME,
-    process.env.DB_USER,
-    process.env.DB_PASSWORD,
-    {
-    host: 'localhost',    //host name
-    dialect: 'mysql',    //'mysql' ,'mariadb', 'postgres','mssql'
-    port: 3306.          //port number
+//////// Create a sequelize instance with the database connection details //////////////////////////////////////////////////////////////////
+const sequelize = process.env.JAWSDB_URL   //////////JawsDB is a MySQL/Maria DBaaS for no custom coding and easy usage /////////////////////
+    ? new Sequelize(process.env.JAWSDB_URL)
+    : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
+        host: 'localhost',
+        dialect: 'mysql',
+        dialectOptions: {
+            decimalNumbers: true,
+        },
     });
-    module.exports = sequelize;    //Export the sequelize object
+
+module.exports = sequelize;    //Export the sequelize object
