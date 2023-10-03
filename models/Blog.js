dee905ab0033bcc5821dcd5a4a4cf0a4662efbcf
Kevin Rice <reatriniti@gmail.com>
2023-10-02 20:14:59 -0400

    completed models/Blog.js file and added public/css, images and js folders

---
+++ models/Blog.js
@@ -0,0 +1,42 @@
+const { Model, DataTypes } = require("sequelize");
+const sequelize = require("../config/connections");
+
+class Blog extends Model {}
+
+Blog.init(
+    {
+        id: {
+            type: DataTypes.INTERGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true,
+        },
+        title: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        content: {
+            type: DataTypes.TEXT,
+            allowNull: false,
+            validate: {
+                len: [1],
+            },
+        },
+        user_id: {
+            type: DataTypes.INTEGER,
+            references: {
+                model: "user",
+                key: "id",
+            },
+        },
+    },
+    {
+        sequelize,
+        timestamps: true,
+        freezeTableName: true,
+        underscored: true,
+        modelName: "blog",
+    }
+);
+
+module.exports = Blog;
