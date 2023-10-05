579d7a5e99160f5009a5d4d816fc6d45b97f352e
Kevin Rice <reatriniti@gmail.com>
2023-10-04 20:05:34 -0400

    created and updated main.handlebars file  and defined SECRET by adding the variable to .env file

--- models/User.js
+++ models/User.js
@@ -1,10 +1,11 @@
-const { Model, DataTypes } = require("sequelize");
-const sequelize = require("../config/connections");
-const bcryptjs = require("bcrypt");
+console.log("Sequelize version:", require('sequelize').version);
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connections');
+const bcrypt = require('bcrypt');
 
 class User extends Model {
     checkPassword(loginPW) {
-        return bcryptjs.compareSync(loginPW, this.password);
+        return bcrypt.compareSync(loginPW, this.password);
     }
 }
 
@@ -35,21 +36,19 @@ User.init(
     {
         hooks: {
             async beforeCreate(newUser) {
-                newUser.password = await bcryptjs.hash(newUser.password, 10);
+                newUser.password = await bcrypt.hash(newUser.password, 10);
                 return newUser;
             },
             async beforeUpdate(updatedUser) {
-                updatedUser.password = await bcryptjs.hash(updatedUser.password, 10);
+                updatedUser.password = await bcrypt.hash(updatedUser.password, 10);
                 return updatedUser;
             },
-        },
-    },
-    {
+        },    
         sequelize,
         timestamps: false,
         freezeTableName: true,
         underscored: true,
-        modelName: "user",
+        modelName: 'user',
     }
 );
 
