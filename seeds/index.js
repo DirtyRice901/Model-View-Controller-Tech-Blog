ff6c46025dd37cf3b4b13bf4dd80e6a0cd4196e9
Kevin Rice <reatriniti@gmail.com>
2023-10-22 18:27:13 -0400

    update hbs files

--- seeds/index.js
+++ seeds/index.js
@@ -1,3 +1,4 @@
+/////////////////// Import the seed data /////////////////////////////////////////////////////////////////////////////////////////////////
 const seedUsers = require("./userData");
 const seedBlogs = require("./blogData");
 const seedComments = require("./commentData");
@@ -6,3 +7,18 @@ const seedComments = require("./commentData");
 const sequelize = require("../config/connection");
 
 /////////////////// Function that seeds all tables with sample data //////////////////////////////////////////////////////////////////////////
+const seedAll = async () => {
+    await sequelize.sync({ force: true });
+    console.log('\n----- DATABASE SYNCED -----\n'); // This is just to let you know that the database synced correctly.//
+    await seedUsers();
+    console.log('\n----- USERS SEEDED -----\n');
+    await seedBlogs();
+    console.log('\n----- BLOGS SEEDED -----\n');
+    await seedComments();
+    console.log('\n----- COMMENTS SEEDED -----\n');
+    /////////////////// Exit the process ////////////////////////////////////////////////////////////////////////////////////////////////////
+    process.exit(0);
+};
+
+/////////////////// Call the function to seed all tables /////////////////////////////////////////////////////////////////////////////////////
+seedAll();
